7a91bd274ac9dfb602d1ae5650b54822601f17c3
TulasiSaiGorantla <tulasisai61@gmail.com>
2024-12-07 22:54:51 +0530

    Add files via upload

---
+++ SpringBootRegistrationLogin/src/main/java/net/codejava/AppController.java
@@ -0,0 +1,93 @@
+package net.codejava;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepo.save(user);
+        return "register_success";
+    }
+
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+        return "welcome";
+    }
+
+    @GetMapping("/home")
+    public String Home(Model model) {
+        model.addAttribute("Home");
+        return "home";
+    }
+
+    @GetMapping("/exam")
+    public String Exam(Model model) {
+        model.addAttribute("Exam");
+        return "Exam";
+    }
+
+    @GetMapping("/welcome")
+    public String Welcome(Model model) {
+        model.addAttribute("welcome");
+        return "welcome";
+    }
+    @GetMapping("/sde")
+    public String Software(Model model) {
+    	model.addAttribute("sde");
+    	return "Software_Dev";
+    }
+    @GetMapping("/ds")
+    public String DataScience(Model model) {
+    	model.addAttribute("ds");
+    	return "datascience";
+    }
+
+    @GetMapping("/profile")
+    public String Profile(Principal principal, Model model) {
+        if (principal == null) {
+            return "redirect:/login"; // Redirect to login if not authenticated
+        }
+
+        // Fetch the user based on the email from Principal
+        String userEmail = principal.getName();
+        User user = userRepo.findByEmail(userEmail);
+
+        if (user == null) {
+            model.addAttribute("error", "User not found");
+            return "error"; // Create an error page if needed
+        }
+
+        // Add user details to the model
+        model.addAttribute("user", user);
+        return "profile"; // Return the profile view
+    }
+}
